045ceadfa2436f30d7512cdddda242a100cee8d4
JoeCleary <joseph.david.cleary@gmail.com>
2017-04-03 09:08:36 -0400

    billy smith

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
 
 public class Entity {
-
+	private int x, y, z;
+	private int xV, yV, zV;
+	
+	public ArrayList<Entity> allEntities = new ArrayList<Entity>();
+	
+	public Entity(){
+		
+	}
+	
+	public void updateAll(){
+		
+	}
+	
+	private void update(){
+		x = x + xV;
+		y = y + yV;
+		z = z + zV;
+		
+		
+	}
 }
