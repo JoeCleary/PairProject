1e9ae31a33d365002087fb7215ef255b500c15f0
FaithSki <ccvpieman@gmail.com>
2017-04-05 09:09:30 -0400

    nerd

--- src/EnemyBear.java
+++ src/EnemyBear.java
@@ -1,4 +1,17 @@
 // strategic bomber- moves in a straight line, drops cluster bombs as it approaches the player
 public class EnemyBear extends EnemyPlane{
 
+	private int x;
+	private int y;
+	private int z;
+	private int pointValue;
+	
+	public EnemyBear(int nX, int nY, int nZ) {
+		super(nX, nY, nZ);
+		x = nX;
+		y = nX;
+		// z = back
+		
+	}
+	
 }
