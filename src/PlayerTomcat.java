caa5108771dec59707b5a81b77b5d5aa5625b5e4
JoeCleary <joseph.david.cleary@gmail.com>
2017-04-05 09:02:47 -0400

    griphub

---
+++ src/PlayerTomcat.java
@@ -0,0 +1,7 @@
+public class PlayerTomcat extends Entity{
+	//z should always be 0 for the player
+	
+	public PlayerTomcat(int nX, int nY, int nZ, int nxV, int nyV, int nzV) {
+		super(nX, nY, nZ);
+	}
+}
