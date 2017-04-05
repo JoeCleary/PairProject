85f3eebbf48176a1701ff7df589b86e9ca06f457
FaithSki <ccvpieman@gmail.com>
2017-04-05 09:09:15 -0400

    boy did i

--- src/EnemyPlane.java
+++ src/EnemyPlane.java
@@ -1,5 +1,12 @@
 
 public class EnemyPlane extends Entity{
 
-	
+	private int pointValue;
+	private int x;
+	private int y;
+	private int z;
+
+	public EnemyPlane(int nX, int nY, int nZ) {
+		super(nX, nY, nZ);
+	}
 }
