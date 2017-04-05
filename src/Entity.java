c1f09184841a371267f0a2a91c603163add19cd1
JoeCleary <joseph.david.cleary@gmail.com>
2017-04-05 09:01:24 -0400

    h

--- src/Entity.java
+++ src/Entity.java
@@ -1,24 +1,30 @@
+import java.awt.image.*;
 import java.util.ArrayList;
 
 public class Entity {
 	private int x, y, z;
 	private int xV, yV, zV;
 	
-	public ArrayList<Entity> allEntities = new ArrayList<Entity>();
+	public static ArrayList<Entity> allEntities = new ArrayList<Entity>();
 	
-	public Entity(){
-		
+	public Entity(int nX, int nY, int nZ){
+		x = nX;
+		y = nY;
+		z = nZ;
 	}
 	
-	public void updateAll(){
-		
+	public static void updateAll(){
+		for(Entity e : allEntities)
+			e.update();
 	}
 	
 	private void update(){
 		x = x + xV;
 		y = y + yV;
 		z = z + zV;
-		
+	}
+	
+	public VolatileImage getSprite(){
 		
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,30 @@
-public class Main {
+import java.awt.*;
 
+import javax.swing.*;
+
+public class Main {
+	JFrame frame;
+	JPanel panel;
+	
+	public static void main(String args[]){
+		new Main().startGame();
+	}
+	
+	public void startGame(){
+		frame = new JFrame();
+		
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		panel = new JPanel(){
+			public void paintComponent(Graphics g){
+				super.paintComponent(g);
+				
+				//g.
+			}
+		};
+		
+		
+		
+		frame.show
+	}
 }
