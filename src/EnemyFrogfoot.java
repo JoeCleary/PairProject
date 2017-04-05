c11e5faf5148f958f4e76c6fd36b81a60a72325c
FaithSki <ccvpieman@gmail.com>
2017-04-05 08:46:23 -0400

    hmmmmmmmmmmmmmmmmmm

--- src/EnemyFrogfoot.java
+++ src/EnemyFrogfoot.java
@@ -1,4 +1,4 @@
-// jet- comes from the front, uses cannons
+// jet- comes from the front, flies straight, fires a missile when half-way to the player
 public class EnemyFrogfoot extends EnemyPlane{
 
 }
