630c3a5c9c36982766706fbb97a3d5450de2fde4
FaithSki <ccvpieman@gmail.com>
2017-04-06 08:42:27 -0400

    prett

--- src/EnemyFlanker.java
+++ src/EnemyFlanker.java
@@ -1,4 +1,16 @@
 // jet- comes from the front, flies straight until half-way to the player, then launches missile & moves diagonally to one corner of the screen
 public class EnemyFlanker extends EnemyPlane{
 
+	private int x;
+	private int y;
+	private int z;
+	
+	public EnemyFlanker(int nX, int nY, int nZ) {
+		super(nX, nY, nZ);
+		x = nX;
+		y = nX;
+		// z = furthest from camera
+		
+	}
+	
 }
