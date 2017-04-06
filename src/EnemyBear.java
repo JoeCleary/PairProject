3d10c463ca24c711c482313122abe8dd2fb299f6
FaithSki <ccvpieman@gmail.com>
2017-04-06 08:41:50 -0400

    f

--- src/EnemyBear.java
+++ src/EnemyBear.java
@@ -4,14 +4,13 @@ public class EnemyBear extends EnemyPlane{
 	private int x;
 	private int y;
 	private int z;
-	private int pointValue;
 	
 	public EnemyBear(int nX, int nY, int nZ) {
 		super(nX, nY, nZ);
 		x = nX;
 		y = nX;
-		// z = back
+		// z = furthest from camera
 		
 	}
-	
+		
 }
