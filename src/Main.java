b5241aad026d1652275c285f3d093be89e4687ce
JoeCleary <joseph.david.cleary@gmail.com>
2017-04-10 16:05:41 -0400

    image scales up and down

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.*;
 import java.io.*;
 
@@ -9,6 +11,14 @@ public class Main {
 	JFrame frame;
 	JPanel panel;
 	
+	boolean up = false, down = false;
+	
+	int w = 300;
+	
+	
+	
+	
+	
 	public static void main(String args[]){
 		new Main().startGame();
 	}
@@ -18,26 +28,65 @@ public class Main {
 		
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		
+		
+		
 		panel = new JPanel(){
 			public void paintComponent(Graphics g){
 				super.paintComponent(g);
 				
-				BufferedImage img = new BufferedImage(100, 100, BufferedImage.SCALE_DEFAULT);
+				BufferedImage img = new BufferedImage(100, 100, BufferedImage.SCALE_FAST);
 				
 				try {
 					img = ImageIO.read(new File("res/img.png"));
 				}catch (IOException e) {}
 				
+				if(up){
+					w++;
+				}else if(down){
+					w--;
+					
+				}
 				
-				
-				g.drawImage(img, 0, 0, 300, 300, null);
+				g.drawImage(img, 0, 0, w, w, null);
 			}
 		};
+
+		frame.addKeyListener(new KeyListener(){
+
+			public void keyPressed(KeyEvent ke) {
+				if(ke.getKeyCode() == KeyEvent.VK_UP)
+					up = true;
+				
+				if(ke.getKeyCode() == KeyEvent.VK_DOWN)
+					down = true;
+				
+				panel.repaint();
+			}
+
+			
+			public void keyReleased(KeyEvent ke) {
+				if(ke.getKeyCode() == KeyEvent.VK_UP)
+					up = false;
+				
+				if(ke.getKeyCode() == KeyEvent.VK_DOWN)
+					down = false;
+				
+				panel.repaint();
+			}
+
+			
+			public void keyTyped(KeyEvent ke) {}
+		});
+		
+		
+		panel.repaint();
 		
 		frame.setContentPane(panel);
 		
 		frame.setVisible(true);
 	}
+	
+	
 	
 }
 
