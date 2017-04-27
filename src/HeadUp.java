d21dbce62c63009ddc475283caafc6169aaf3fcd
FaithSki <ccvpieman@gmail.com>
2017-04-27 09:06:14 -0400

    scary joe i don't like that

--- src/HeadUp.java
+++ src/HeadUp.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ConcurrentModificationException;
@@ -10,30 +11,45 @@ import javax.imageio.ImageIO;
 
 public class HeadUp extends Entity{
 	
-	int score;
-	int hits;
-	int missiles;
+	BufferedImage missileMajor;
+	BufferedImage missileMinor;
 	Font hudText;
 	Graphics gui;
 	//new HeadUp(600,500,1000);
 	public HeadUp(float nX, float nY, float nZ) {
 		super(nX, nY, nZ);
+		hudText = new Font("Impact", Font.PLAIN, 44);
+		
 		try {
 			sprite = ImageIO.read(new File("res/hud.png"));
-		} catch (IOException e) {System.out.println();}
-		gui = sprite.getGraphics();
-		gui.setColor(Color.YELLOW);
-		hudText = new Font("Impact", Font.PLAIN, 44);
-		gui.setFont(hudText);
-		gui.drawString(""+0, 310, 67);
-		gui.drawString(""+0, 810, 67);
-		//gui.drawString("missiles: " + 0, 300, 300);
+			missileMajor = ImageIO.read(new File("res/missilemajor.png"));
+			missileMajor = ImageIO.read(new File("res/missilemajor.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void update(){
 		Graphics gui = sprite.getGraphics();
-		gui.drawString("score: " + test.getScore(), 100, 100);
-		gui.drawString("hits: " + test.getHits(), 100, 300);
+		int score = 421552;//test.getScore();
+		int hits = 13;//test.getScore();
+		int missiles = 33;//test.getScore();
+		gui.setFont(hudText);
+		gui.setColor(Color.YELLOW);
+		gui.drawString("score: " + score, 310, 67);
+		gui.drawString("hits: " + hits, 810, 67);
+		int majorMissiles = missiles/10;
+		int minorMissiles = missiles%10;
+		int outsideCounter = 0;
+		for(int counter = 0; counter < majorMissiles; counter++){
+			System.out.println("DEBUG: drawing a major missile at " + (1500+(counter*3)));
+			gui.drawImage(missileMajor, 1500+(counter*3), 650, null);
+			outsideCounter = counter*5;
+		}
+		for(int counter = 0; counter < minorMissiles; counter++){
+			System.out.println("DEBUG: drawing a minor missile at " + (1500+(counter*1)));
+			gui.drawImage(missileMinor, 1500+(counter*1), 650, null);
+		}
 	}
 
 }
